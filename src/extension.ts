cc46eec652e7adbc510621ecf38b7d1e9a6e8d86
牛客945484650号 <zhaojiuzhou2015@outlook.com>
2024-07-04 02:28:26 +0800

    feat: add css var manager and initiate server

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,9 @@
 import * as vscode from "vscode";
 import { readFileSync } from "fs";
-import path, { join } from "path";
+import { join } from "path";
 import { render } from "ejs";
 import tinycolor from "tinycolor2";
-import {
-  LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node'
-
-const LANGUAGES = [
-  'astro',
-  'svelte',
-  'vue',
-  'vue-html',
-  'vue-postcss',
-  'scss',
-  'postcss',
-  'less',
-  'css',
-  'html',
-  'javascript',
-  'javascriptreact',
-  'typescript',
-  'typescriptreact',
-  'source.css.styled',
-]
-
-let clientInstance: LanguageClient;
+import startServer from './client'
 
 enum BultinTemplateVar {
   remResult = "_REM_RESULT_",
@@ -191,50 +166,6 @@ function init(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("cssAction.pickVariable", showQuickPick)
   );
-}
-
-/**
-   * 启动lsp
-   * @param context
-   */
-function startServer(context: vscode.ExtensionContext) {
-  const serverModule = context.asAbsolutePath(path.join("dist", "server.js"));
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: LANGUAGES.map((language) => ({
-      scheme: 'file',
-      language,
-    })),
-    synchronize: {
-      fileEvents: [
-        vscode.workspace.createFileSystemWatcher('**/*.css'),
-        vscode.workspace.createFileSystemWatcher('**/*.scss'),
-        vscode.workspace.createFileSystemWatcher('**/*.sass'),
-        vscode.workspace.createFileSystemWatcher('**/*.less'),
-      ],
-    },
-  };
-  clientInstance = new LanguageClient(
-    'cssVariables',
-    'CSS Variables Language Server',
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  clientInstance.start();
 }
 
 export function activate(context: vscode.ExtensionContext) {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,6 @@
+/**
+ * great thanks to https://github.com/vunguyentuan/vscode-css-variables and repo owner Vu Nguyen
+ */
 import {
     createConnection,
     TextDocuments,
@@ -12,7 +15,60 @@ import {
     TextDocumentSyncKind,
     InitializeResult
 } from 'vscode-languageserver/node';
-
+import CSSVariableManager, { CSSVariablesSettings, defaultSettings } from './managers/CssVariableManager';
 import {
     TextDocument
 } from 'vscode-languageserver-textdocument';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+const cssVariableManager = new CSSVariableManager();
+
+// ------------------------ Language server lifesycle ------------------------ 
+connection.onInitialize(async (params: InitializeParams) => {
+	const capabilities = params.capabilities;
+  
+	// check editor capacities
+	hasConfigurationCapability = !!(
+	  capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+	  capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+	  capabilities.textDocument &&
+	  capabilities.textDocument.publishDiagnostics &&
+	  capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+  
+	// construct server capacities
+	const result: InitializeResult = {
+	  capabilities: {
+		textDocumentSync: TextDocumentSyncKind.Incremental,
+		completionProvider: {
+		  resolveProvider: true,
+		},
+		diagnosticProvider: {
+			interFileDependencies: false,
+			workspaceDiagnostics: false
+		},
+		definitionProvider: true,
+		hoverProvider: true,
+		colorProvider: true,
+	  },
+	};
+  
+	if (hasWorkspaceFolderCapability) {
+	  result.capabilities.workspace = {
+		workspaceFolders: {
+		  supported: true,
+		},
+	  };
+	}
+	return result;
+  });
+
